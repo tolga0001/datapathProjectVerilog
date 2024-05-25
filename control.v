fcb9b4f10cc9b5d5726b1fd82b6b52d67d0fc2c5
Mehmet Toprak Balıkcı <149236560+MehmetToprakBalikci@users.noreply.github.com>
2024-05-25 09:10:12 +0300

    Blezal only implementation

--- control.v
+++ control.v
@@ -7,16 +7,19 @@ assign rformat=~|in;
 assign lw=in[5]& (~in[4])&(~in[3])&(~in[2])&in[1]&in[0];
 assign sw=in[5]& (~in[4])&in[3]&(~in[2])&in[1]&in[0];
 assign beq=~in[5]& (~in[4])&(~in[3])&in[2]&(~in[1])&(~in[0]);
-assign jalpc =~in[5]& (in[4])&(in[3])&(in[2])&in[1]&in[0];  // 011111 jalpc instuction
 assign regdest=rformat;
-assign alusrc=(lw|sw)&(~brvsig) ;//alusrc is 0 when brvsig is 1
+//assign alusrc=(lw|sw)&(~brvsig) ;//alusrc is 0 when brvsig is 1
+assign alusrc=(lw|sw)
 assign memtoreg=lw;
-assign regwrite=rformat|lw|blezalsig;//added blezalsig to regwrite
+//assign regwrite=(rformat|lw|blezalsig|jalpcsig|nandisig|balvsig|jmxorsig);//added blezalsig to regwrite
+assign regwrite=rformat
 assign memread=lw;
 assign memwrite=sw;
 assign branch=beq;
 assign aluop1=rformat; 
 assign aluop2=beq;
+
+assign jalpc =~in[5]& (in[4])&(in[3])&(in[2])&in[1]&in[0];  // 011111 jalpc instuction
 assign brvsig=(~functcode[5])&(functcode[4])&(~functcode[3])&(functcode[2])&(~functcode[1])&(~functcode[0]); //010100=>20
 assign jmxorsig=(functcode[5])&(~functcode[4])&(~functcode[3])&(~functcode[2])&(functcode[1])&(~functcode[0]); //100010=>34
 assign blezalsig=in[5]&(~in[4])&(~in[3])&in[2]&(~in[1])&(~in[0]);//100100 is the opcode
--- processor.v
+++ processor.v
@@ -70,7 +70,7 @@ wire zout,	//Zero output of ALU
 pcsrc,	//Output of AND gate with Branch and ZeroOut inputs
 
 //Control signals
-regdest,alusrc,memtoreg,regwrite,memread,memwrite,branch,aluop1,aluop0,brvsig,blezalsig,jalpcsig,nandisig,balvsig,jmxorsig;//TODO changed jalpc to jalpcsig here!!
+regdest,alusrc,memtoreg,regwrite,memread,memwrite,branch,aluop1,aluop0,brvsig,blezalsig,jalpcsig,nandisig,balvsig,jmxorsig;
 
 //32-size register file (32 bit(1 word) for each register)
 reg [31:0] registerfile[0:31];
@@ -84,14 +84,14 @@ always @(posedge clk)
 if (memwrite)
 begin 
 //sum stores address,datab stores the value to be written
-//datmem[sum[4:0]+3]=datab[7:0];
-//datmem[sum[4:0]+2]=datab[15:8];
-//datmem[sum[4:0]+1]=datab[23:16];
-//datmem[sum[4:0]]=datab[31:24];
-datmem[outjmxoraddress[4:0]+3]=datab[7:0];
-datmem[outjmxoraddress[4:0]+2]=datab[15:8];
-datmem[outjmxoraddress[4:0]+1]=datab[23:16];
-datmem[outjmxoraddress[4:0]]=datab[31:24];
+datmem[sum[4:0]+3]=datab[7:0];
+datmem[sum[4:0]+2]=datab[15:8];
+datmem[sum[4:0]+1]=datab[23:16];
+datmem[sum[4:0]]=datab[31:24];
+//datmem[outjmxoraddress[4:0]+3]=datab[7:0];//jmxor deciding what data to store
+//datmem[outjmxoraddress[4:0]+2]=datab[15:8];
+//datmem[outjmxoraddress[4:0]+1]=datab[23:16];
+//datmem[outjmxoraddress[4:0]]=datab[31:24];
 end
 
 always @(posedge clk) begin
@@ -114,11 +114,13 @@ always @(posedge clk) begin
 
 assign dataa=registerfile[inst25_21];//Read register 1
 assign datab=registerfile[inst20_16];//Read register 2
-assign blezaland=blezalsig&(dataa<=0 ? 1:0); //added blezaland here Toprak
-assign jmxorxor=dataa^datab; //added jmxorxor here Toprak, TODO check if ^ is xor
+
+//assign blezaland=blezalsig&(dataa[31]==1 ? 1:0); //added blezaland here Toprak 2's complement???
+//assign jmxorxor=dataa^datab; //added jmxorxor here Toprak, TODO check if ^ is xor
+
 always @(posedge clk)
- //registerfile[out1]= regwrite ? out3:registerfile[out1];//Write data to register
- registerfile[outbalvregdst]= regwrite ? outnandimux:registerfile[outbalvregdst];//Write data to register
+ registerfile[out1]= regwrite ? out3:registerfile[out1];//Write data to register
+ //registerfile[outbalvregdst]= regwrite ? outnandimux:registerfile[outbalvregdst];//Write data to register depending on our muxes
 
 //read data from memory, sum stores address
 assign dpack={datmem[sum[5:0]],datmem[sum[5:0]+1],datmem[sum[5:0]+2],datmem[sum[5:0]+3]};
@@ -143,33 +145,34 @@ mult2_to_1_32 mult4(out3, sum,dpack,memtoreg);
 mult2_to_1_32 mult6 (out4, adder1out,adder2out,pcsrc);
 
 // mux with select nandi
-mult2_to_1_32 nandimux(outnandimux,nandiresult,outcombinedmux,nandisig); 
+//mult2_to_1_32 nandimux(outnandimux,nandiresult,outcombinedmux,nandisig); 
 
 // mux with (blezalmux) control added by Toprak
 mult2_to_1_32 blezalmux (outblezalmux, adder1out, adder2out, (blezaland|pcsrc));
 
 // mux with (balv) control added by Tolga
-mult2_to_1_32 balvmux (outbalvmux, outblezalmux, shiftedjump32, ((statusregister[0])&balvsig)); // it will be implemented for balv
+//mult2_to_1_32 balvmux (outbalvmux, outblezalmux, shiftedjump32, ((statusregister[0])&balvsig)); // it will be implemented for balv
 
 // mux with (Brv) control added by Tolga
-mult2_to_1_32 brvmux (outbrvmux, outbalvmux, sum, (brvsig&statusregister[0])); 
+//mult2_to_1_32 brvmux (outbrvmux, outbalvmux, sum, (brvsig&statusregister[0])); 
 
 // mux with (jalpc) control added by Tolga
-mult2_to_1_32 jalpcmux (outjalpcmux, outbrvmux, adder1out, jalpcsig);//changed jalpc to jalpcsig 
+//mult2_to_1_32 jalpcmux (outjalpcmux, outbrvmux, adder1out, jalpcsig);//changed jalpc to jalpcsig 
 
 // mux with control (jmxormux)
-mult2_to_1_32 jmxormux (jmxorout, outjalpcmux, dpack, jmxorsig);
+//mult2_to_1_32 jmxormux (outjmxormux, outjalpcmux, dpack, jmxorsig);
 
 // mux with control (alu result and address connected by jmxorsig) added by Toprak
-mult2_to_1_32 jmxoraddressmux(outjmxoraddress, sum, jmxorxor, jmxorsig);
+//mult2_to_1_32 jmxoraddressmux(outjmxoraddress, sum, jmxorxor, jmxorsig);
 
 // mux with control (jmxor, jalpc,(balvsig&status[0]) selecting data to be written)
-mult2_to_1_32 combineddatawritemux(outcombinedmux, out3, adder1out, (jmxorsig|jalpcsig|(balvsig&statusregister[0])));
+//mult2_to_1_32 combineddatawritemux(outcombinedmux, out3, adder1out, (jmxorsig|jalpcsig|(balvsig&statusregister[0])));
 
 // load pc
 always @(negedge clk)
 //pc=out4;
-pc=outblezalmux;
+pc=outblezalmux;//temporary
+//pc=outjmxormux; //last version!!
 
 // alu, adder and control logic connections
 
@@ -184,7 +187,6 @@ adder add1(pc,32'h4,adder1out);
 //adder which adds PC+4 and 2 shifted sign-extend result
 adder add2(adder1out,sextad,adder2out);
 
-//Control unit TODO:jalpc eklencek
 control cont(instruc[31:26],functcode,regdest,alusrc,memtoreg,regwrite,memread,memwrite,branch,
 aluop1,aluop0,brvsig,blezalsig,jalpcsig,nandisig,balvsig,jmxorsig);
 
@@ -192,7 +194,7 @@ aluop1,aluop0,brvsig,blezalsig,jalpcsig,nandisig,balvsig,jmxorsig);
 signext sext(instruc[15:0],extad);
 
 // zero extend unit
-zeroext zext(instruc[15:0],zeroextaddr);
+//zeroext zext(instruc[15:0],zeroextaddr);
 
 // sign extend for nandi instruction
 signext sextnandi(instruc[15:0],signextnandi);
